092bb4d482c6560167b3b8fead096b6af6e28f21
ozturk sirin <ozturk2002.zri@gmail.com>
2023-07-27 16:27:21 +0300

    bottom navigation added

--- src/Pages/More/Menu.js
+++ src/Pages/More/Menu.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { styles } from '../../Assets/Styles/Pages/menuStyle'
 import { Image, View, Text } from 'react-native'
 import User from '../../Assets/Images/icons/userImg.png'
@@ -11,18 +11,45 @@ import privacy from '../../Assets/Images/icons/privacy.png'
 import folder from '../../Assets/Images/icons/folder.png'
 import help from '../../Assets/Images/icons/help.png'
 import invite from '../../Assets/Images/icons/invite.png'
+import logout from '../../Assets/Images/icons/logout.png'
+import { useDispatch, useSelector } from 'react-redux'
+import { save } from '../../Redux/Slices/authSlice'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import auth from '@react-native-firebase/auth'
+const Menu = (props) => {
+    const { navigation } = props
+
+    const dispatch = useDispatch();
 
 
+    const user = useSelector(state => state.user.user);
 
+    useEffect(() => {
+        console.log('user', user);
+    }, []);
 
-const Menu = () => {
+    const logOut = () => {
+
+        try {
+            auth().signOut();
+            AsyncStorage.removeItem('USER');
+            dispatch(save(null));
+            if (user) {
+                navigation.navigate('Login');
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <View style={styles.container}>
             <View style={{ flexDirection: 'row', }}>
                 <Image source={User} style={styles.user} />
                 <View style={styles.textArea}>
                     <Text style={styles.name}>Ozturk Sirin</Text>
-                    <Text style={styles.info}>ozturksirininfo@gmail.com</Text>
+                    <Text style={styles.info}>{user?.email}</Text>
                 </View>
             </View>
             <View>
@@ -34,6 +61,8 @@ const Menu = () => {
                 <MenuItem image={folder} text={'Data Usage'} onPress={null} />
                 <MenuItem image={help} text={'Help'} onPress={null} />
                 <MenuItem image={invite} text={'Invite Your Friends'} onPress={null} />
+                <MenuItem image={logout} text={'Log out'} onPress={logOut} />
+
             </View>
         </View>
     )
--- src/Router/App.js
+++ src/Router/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Chat from '../Pages/Chat/Chat';
@@ -10,26 +11,90 @@ import ProfileCreate from '../Pages/Profile/ProfileCreate';
 import Register from '../Pages/Auth/Register';
 import Menu from '../Pages/More/Menu';
 import Messages from '../Pages/Messages/Messages';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import chats from '../Assets/Images/icons/chats.png'
+import accountTab from '../Assets/Images/icons/accountTab.png'
+import more from '../Assets/Images/icons/more.png'
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
+const HomeStackScreen = () => {
+  return (
+    <Stack.Navigator initialRouteName='Login'>
+      <Stack.Screen name='Login' component={Login} />
+      <Stack.Screen name='Register' component={Register} />
+      <Stack.Screen name='ProfileCreate' component={ProfileCreate} />
+
+      <Stack.Screen name="Contacts" component={Contacts} />
+      <Stack.Screen name="Messages" component={Messages} />
+      <Stack.Screen name='Menu' component={Menu} />
+      <Stack.Screen name='Chat' component={Chat} />
+    </Stack.Navigator>
+  );
+}
 
 const Router = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='Login'>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Register" component={Register} />
-          <Stack.Screen name='Menu' component={Menu} />
-          <Stack.Screen name='ProfileCreate' component={ProfileCreate} />
-          <Stack.Screen name="Contacts" component={Contacts} />
-          <Stack.Screen name="Messages" component={Messages} />
-          <Stack.Screen name="Chat" component={Chat} />
-        </Stack.Navigator>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarStyle: {
+              backgroundColor: '#0F1828',
+              elevation: 15,
+              borderTopWidth: 0,
+              paddingBottom: 4,
+            },
+          }}
+        >
+          <Tab.Screen name="Contacts1"
+            component={HomeStackScreen}
+            screenOptions={{ headerShown: false }}
+            options={{
+              tabBarIcon: ({ focused }) => (
+                <Image
+                  source={accountTab}
+                  resizeMode='contain'
+                  style={{
+                    tintColor: focused ? '#fff' : '#748c94',
+                  }}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen name="Messages" component={Messages}
+            options={{
+              tabBarIcon: ({ focused }) => (
+                <Image
+                  source={chats}
+                  resizeMode='contain'
+                  style={{
+                    tintColor: focused ? '#fff' : '#748c94',
+                  }}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen name='More' component={Menu}
+            options={{
+              tabBarIcon: ({ focused }) => (
+                <Image
+                  source={more}
+                  resizeMode='contain'
+                  style={{
+                    tintColor: focused ? '#fff' : '#748c94',
+                  }}
+                />
+              ),
+            }}
+          />
+        </Tab.Navigator>
       </NavigationContainer>
     </Provider>
-  );
+  )
 };
 
 export default Router;
