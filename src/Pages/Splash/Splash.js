7d90a4d14ca556e08cf69f69f791ad0b0a653a26
ozturk sirin <ozturk2002.zri@gmail.com>
2023-11-17 08:50:50 +0300

    Refactor Splash.js file

--- src/Pages/Splash/Splash.js
+++ src/Pages/Splash/Splash.js
@@ -1,53 +1,53 @@
-import React, { useEffect } from 'react'
-import { styles } from '../../Assets/Styles/Pages/splashStyle'
-import { View, Image, Text } from 'react-native'
-import splash from '../../Assets/Images/splash.png'
-import { useDispatch } from 'react-redux'
-import { save, authCheck } from '../../Redux/Slices/authSlice'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import React, {useEffect} from 'react';
+import {styles} from '../../Assets/Styles/Pages/splashStyle';
+import {View, Image, Text} from 'react-native';
+import splash from '../../Assets/Images/splash.png';
+import {useDispatch} from 'react-redux';
+import {save, authCheck} from '../../Redux/Slices/authSlice';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Splash = (props) => {
-    const { navigation } = props
-    const dispatch = useDispatch();
+const Splash = props => {
+  const {navigation} = props;
+  const dispatch = useDispatch();
 
-    const getUser = async () => {
-        try {
-            const user_val = await AsyncStorage.getItem('USER');
-            if (user_val != null) {
-                const user = JSON.parse(user_val);
-                dispatch(save(user));
-                dispatch(authCheck(true));
-                setTimeout(() => {
-                    navigation.navigate('Matchs');
-                }, 3000);
-            }
-            else {
-                dispatch(save(null));
-                dispatch(authCheck(false));
-                setTimeout(() => {
-                    navigation.navigate('Login')
-                }, 3000);
-            }
-        }
-        catch (error) {
-            dispatch(save(null));
-            dispatch(authCheck(false));
-            navigation.navigate('Login');
-            console.log(error);
-        }
+  const getUser = async () => {
+    try {
+      const user_val = await AsyncStorage.getItem('USER');
+      if (user_val != null) {
+        const user = JSON.parse(user_val);
+        dispatch(save(user));
+        dispatch(authCheck(true));
+        setTimeout(() => {
+          navigation.replace('Matchs');
+        }, 3000);
+      } else {
+        dispatch(save(null));
+        dispatch(authCheck(false));
+        setTimeout(() => {
+          navigation.replace('Login');
+        }, 3000);
+      }
+    } catch (error) {
+      dispatch(save(null));
+      dispatch(authCheck(false));
+      navigation.replace('Login');
+      console.log(error);
     }
-    useEffect(() => {
-        getUser()
-    }, [])
-    return (
-        <View style={styles.container}>
-            <View style={styles.imgArea}>
-                <Image source={splash} alt='splash' style={styles.img} />
-            </View>
-            <View style={styles.textArea}>
-                <Text style={styles.text}>Connect easily with your family and friends over countries</Text>
-            </View>
-        </View >
-    )
-}
-export default Splash
+  };
+  useEffect(() => {
+    getUser();
+  }, []);
+  return (
+    <View style={styles.container}>
+      <View style={styles.imgArea}>
+        <Image source={splash} alt="splash" style={styles.img} />
+      </View>
+      <View style={styles.textArea}>
+        <Text style={styles.text}>
+          Connect easily with your family and friends over countries
+        </Text>
+      </View>
+    </View>
+  );
+};
+export default Splash;
--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { styles } from '../Assets/Styles/routerStyle';
-import { Image, BackHandler, View, Text, ToastAndroid } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import React, {useState, useEffect} from 'react';
+import {styles} from '../Assets/Styles/routerStyle';
+import {Image, BackHandler, View, Text, ToastAndroid} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 import Chat from '../Pages/Chat/Chat';
 import Login from '../Pages/Auth/Login';
@@ -12,167 +12,193 @@ import Menu from '../Pages/More/Menu';
 import Messages from '../Pages/Messages/Messages';
 import Splash from '../Pages/Splash/Splash';
 
-import chats from '../Assets/Images/icons/chats.png'
-import accountTab from '../Assets/Images/icons/accountTab.png'
-import more from '../Assets/Images/icons/more.png'
-import { useSelector } from 'react-redux';
+import chats from '../Assets/Images/icons/chats.png';
+import accountTab from '../Assets/Images/icons/accountTab.png';
+import more from '../Assets/Images/icons/more.png';
+import {useSelector} from 'react-redux';
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Account from '../Pages/More/Account/Account';
 import ForgotPassword from '../Pages/Auth/ForgotPassword';
 const Tab = createBottomTabNavigator();
 
 const HomeStackScreen = () => {
-    return (
-        <Tab.Navigator
-            screenOptions={{
-                tabBarStyle: {
-                    backgroundColor: '#0F1828',
-                    elevation: 15,
-                    borderTopWidth: 0,
-                    paddingBottom: 4,
-                },
-                headerStyle: {
-                    backgroundColor: '#0F1828',
-                    elevation: 0,
-                    shadowOpacity: 0,
-                    borderBottomWidth: 0,
-                },
-                headerTitleStyle: {
-                    color: '#fff'
-                },
-            }}
-
-        >
-            <Tab.Screen name="Matchs"
-                component={Contacts}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={accountTab}
-                            resizeMode='contain'
-                            style={{
-                                tintColor: focused ? '#fff' : '#748c94',
-                            }}
-                        />
-                    ),
-                }}
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: '#0F1828',
+          elevation: 15,
+          borderTopWidth: 0,
+          paddingBottom: 4,
+        },
+        headerStyle: {
+          backgroundColor: '#0F1828',
+          elevation: 0,
+          shadowOpacity: 0,
+          borderBottomWidth: 0,
+        },
+        headerTitleStyle: {
+          color: '#fff',
+        },
+      }}>
+      <Tab.Screen
+        name="Matchs"
+        component={Contacts}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={accountTab}
+              resizeMode="contain"
+              style={{
+                tintColor: focused ? '#fff' : '#748c94',
+              }}
             />
-            <Tab.Screen name="Messages" component={Messages}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={chats}
-                            resizeMode='contain'
-                            style={{
-                                tintColor: focused ? '#fff' : '#748c94',
-                            }}
-                        />
-                    ),
-                }}
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Messages"
+        component={Messages}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={chats}
+              resizeMode="contain"
+              style={{
+                tintColor: focused ? '#fff' : '#748c94',
+              }}
             />
-            <Tab.Screen name='More' component={Menu}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={more}
-                            resizeMode='contain'
-                            style={{
-                                tintColor: focused ? '#fff' : '#748c94',
-                            }}
-                        />
-                    ),
-                }}
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="More"
+        component={Menu}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={more}
+              resizeMode="contain"
+              style={{
+                tintColor: focused ? '#fff' : '#748c94',
+              }}
             />
-        </Tab.Navigator>
-    )
-
-}
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const Router = () => {
-    const isAuth = useSelector((state) => state.user.isAuth);
-    const [backButtonPressedOnce, setBackButtonPressedOnce] = useState(false);
+  const isAuth = useSelector(state => state.user.isAuth);
+  // const [backButtonPressedOnce, setBackButtonPressedOnce] = useState(false);
 
-    useEffect(() => {
-        const backAction = () => {
-            if (backButtonPressedOnce) {
-                BackHandler.exitApp();
-            }
-            else {
-                setBackButtonPressedOnce(true);
-                setTimeout(() => {
-                    setBackButtonPressedOnce(false);
-                }, 3000);
-                ToastAndroid.show('Press back again to exit', ToastAndroid.SHORT);
-            }
-            return true;
-        };
-        const backHandler = BackHandler.addEventListener(
-            'hardwareBackPress', backAction,
-        );
-        return () => backHandler.remove();
-    }, [backButtonPressedOnce]);
+  // useEffect(() => {
+  //     const backAction = () => {
+  //         if (backButtonPressedOnce) {
+  //             BackHandler.exitApp();
+  //         }
+  //         else {
+  //             setBackButtonPressedOnce(true);
+  //             setTimeout(() => {
+  //                 setBackButtonPressedOnce(false);
+  //             }, 3000);
+  //             ToastAndroid.show('Press back again to exit', ToastAndroid.SHORT);
+  //         }
+  //         return true;
+  //     };
+  //     const backHandler = BackHandler.addEventListener(
+  //         'hardwareBackPress', backAction,
+  //     );
+  //     return () => backHandler.remove();
+  // }, [backButtonPressedOnce]);
 
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName='Splash'
-                screenOptions={{
-                    headerStyle: {
-                        backgroundColor: '#0F1828',
-                        elevation: 0,
-                        shadowOpacity: 0,
-                        borderBottomWidth: 0,
-                    },
-                    headerTitleStyle: {
-                        color: '#fff',
-                    },
-                    headerTintColor: '#fff',
-                }}
-            >
-                {
-                    !isAuth ? (
-                        <>
-                            <Stack.Screen name='Splash' component={Splash} options={{ headerShown: false }} />
-                            <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
-                            <Stack.Screen name='Register' component={Register} options={{ title: 'Register' }} />
-                            <Stack.Screen name='ProfileCreate' component={ProfileCreate} options={{ title: 'Profile Create' }} />
-                            <Stack.Screen name='ForgotPassword' component={ForgotPassword} options={{ title: 'Forgot Password' }} />
-                        </>
-                    ) :
-                        (
-                            <>
-                                <Stack.Screen name='Home' component={HomeStackScreen} options={{ headerShown: false }} />
-                                <Stack.Screen name='Chat' component={Chat}
-                                    options={({ route }) => ({
-                                        headerTitle: () => (
-                                            <View style={styles.area}>
-                                                <Image
-                                                    style={styles.image}
-                                                    source={
-                                                        route.params.users.image
-                                                            ? { uri: route.params.users.image }
-                                                            : null
-                                                    }
-                                                />
-                                                <Text
-                                                    style={styles.userName}
-                                                >{route.params.users.firstName + ' ' + route.params.users.lastName}</Text>
-                                            </View>
-                                        ),
-                                    })}
-
-                                />
-                                <Stack.Screen name="Messages" component={Messages} />
-                                <Stack.Screen name='Menu' component={Menu} />
-                                <Stack.Screen name='Account' component={Account} />
-                            </>
-                        )
-                }
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#0F1828',
+            elevation: 0,
+            shadowOpacity: 0,
+            borderBottomWidth: 0,
+          },
+          headerTitleStyle: {
+            color: '#fff',
+          },
+          headerTintColor: '#fff',
+        }}>
+        {!isAuth ? (
+          <>
+            <Stack.Screen
+              name="Splash"
+              component={Splash}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Register"
+              component={Register}
+              options={{title: 'Register'}}
+            />
+            <Stack.Screen
+              name="ProfileCreate"
+              component={ProfileCreate}
+              options={{title: 'Profile Create'}}
+            />
+            <Stack.Screen
+              name="ForgotPassword"
+              component={ForgotPassword}
+              options={{title: 'Forgot Password'}}
+            />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              name="Home"
+              component={HomeStackScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Chat"
+              component={Chat}
+              options={({route}) => ({
+                headerTitle: () => (
+                  <View style={styles.area}>
+                    <Image
+                      style={styles.image}
+                      source={
+                        route.params.users.image
+                          ? {uri: route.params.users.image}
+                          : null
+                      }
+                    />
+                    <Text style={styles.userName}>
+                      {route.params.users.firstName +
+                        ' ' +
+                        route.params.users.lastName}
+                    </Text>
+                  </View>
+                ),
+              })}
+            />
+            <Stack.Screen name="Messages" component={Messages} />
+            <Stack.Screen name="Menu" component={Menu} />
+            <Stack.Screen name="Account" component={Account} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default Router;
